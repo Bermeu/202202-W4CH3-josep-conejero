295a106e6cd84a8cc4ba39ff2d1044ac308817b6
Josep Conejero <bermeu@gmail.com>
2022-02-02 18:29:04 +0100

    Added favicon and app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <!-- El siguiente elemento se oculta añadiéndole la clase "off" -->
+      <span class="message">Calling...</span>
+      <main class="phone">
+        <div class="keyboard-container">
+          <ol class="keyboard">
+            <li><button class="key">1</button></li>
+            <li><button class="key">2</button></li>
+            <li><button class="key">3</button></li>
+            <li><button class="key">4</button></li>
+            <li><button class="key">5</button></li>
+            <li><button class="key">6</button></li>
+            <li><button class="key">7</button></li>
+            <li><button class="key">8</button></li>
+            <li><button class="key">9</button></li>
+            <li><button class="key">0</button></li>
+            <li><button class="key big">delete</button></li>
+          </ol>
+        </div>
+        <div class="actions">
+          <span class="number">667359961</span>
+          <!-- El botón de llamar debe tener la clase "activo" cuando -->
+          <!-- el número de teléfono tiene 9 cifras -->
+          <a href="#" class="call">Call</a>
+          <!-- Sólo se tiene que ver un botón u otro -->
+          <a href="#" class="hang active">Hang</a>
+        </div>
+      </main>
     </div>
   );
 }
